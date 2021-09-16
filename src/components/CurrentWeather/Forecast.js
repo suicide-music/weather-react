94b68d5e64585c45f9d12b02356d98a7abd2a956
sucide-music <vi.koluzaeva@gmail.com>
2021-09-16 19:18:58 +0100

    Added useEffect to update Forecast

--- src/components/CurrentWeather/Forecast.js
+++ src/components/CurrentWeather/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 import Loader from "react-loader-spinner";
@@ -6,6 +6,10 @@ import Loader from "react-loader-spinner";
 export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
